5d02691c320e19d97e3be008cd3d98fd81838b08
Eric Iacutone <eric.iacutone@gmail.com>
2013-07-19 13:08:34 -0400

    articles index displaying properly

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,8 @@ class ArticlesController < ApplicationController
 	include ArticlesHelper
 
 	def index
-		@articles = Article.select('DISTINCT created_at, nytimes_title, nytimes_link, huff_title, huff_link, nypost_title, nypost_link, fox_title, fox_link')
+		distinct_articles = Article.select('DISTINCT id, created_at, nytimes_title, nytimes_link, huff_title, huff_link, nypost_title, nypost_link, fox_title, fox_link')
+		@articles = distinct_articles.order('id DESC')
 	end
 
 	def show
